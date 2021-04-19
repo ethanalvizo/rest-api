fdca52ce3481b7040fa66d2192a75cc2fe042a3b
ethanalvizo <55671206+ethanalvizo@users.noreply.github.com>
2021-04-19 18:23:25 -0400

    Testing deployment to netlify

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import Form from './components/Form';
 import ToDoList from './components/ToDoList'
-import firebase from './util/firebase';
 
 function App() {
   return (
