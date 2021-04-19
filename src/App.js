9c0a087aa1f150aaf4b5f7b70633faef3a6ac192
ethanalvizo <55671206+ethanalvizo@users.noreply.github.com>
2021-04-19 16:51:01 -0400

    Displaying information from firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Form from './components/Form';
+import ToDoList from './components/ToDoList'
+import firebase from './util/firebase';
 
 function App() {
   return (
     <div >
       <h1>Todo</h1>
       <Form />
+      <ToDoList/>
     </div>
   );
 }
