d5e912c036fcda0602d6d7bd2cc46d2bcd639cb1
ethanalvizo <55671206+ethanalvizo@users.noreply.github.com>
2021-04-18 23:33:28 -0400

    Basic create function to firebase

---
+++ src/components/Form.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import firebase from '../util/firebase';
+
+export default function Form() {
+    const [title, setTitle] = useState('');
+
+    const handleOnChange = (e) => {
+        setTitle(e.target.value);
+    }
+
+    const createToDo = () => {
+        const toDoRef = firebase.database().ref("Todo");
+        const todo = {
+            title,
+            complete: false,
+
+        };
+
+        toDoRef.push(todo)
+    }
+
+    return (
+        <div>
+            <input type="text" onChange={handleOnChange} value={title}/>
+            <button onClick={createToDo}>Add Todo</button>
+        </div>
+    )
+}
---
+++ src/util/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCS0fekd2vYTHayIH9_n5WAhxDHXLEtCoM",
+    authDomain: "wellness-dashboard-df602.firebaseapp.com",
+    projectId: "wellness-dashboard-df602",
+    storageBucket: "wellness-dashboard-df602.appspot.com",
+    messagingSenderId: "340329504693",
+    appId: "1:340329504693:web:9a30fb5ad75b521e49c3fc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+export default firebase;
